48bd670f2d77f7cfed175fe5f8845a998be38ebb
Alejandro Phocco <aphoccot@unsa.edu.pe>
2023-05-07 14:45:33 -0500

    agregado mi nombre

--- Practica.java
+++ Practica.java
@@ -2,5 +2,6 @@ public class Practica {
     public static void main(String[] args) {
       System.out.println("hola mundo");
       System.out.println("bienvenido al curso");
+      System.out.println("Alejandro Josue Phocco Tapia");
     }
 }
